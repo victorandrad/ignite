3149326f284eabb3220c1ee2814a08d1effc7ade
Victor Andrade <victor.andrade62@hotmail.com>
2021-12-08 20:23:46 -0300

    primeiro commit ignite

---
+++ reactjs/01-github-explorer/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const reactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        isDevelopment && new reactRefreshWebpackPlugin(),
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ].filter(Boolean),
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
+}
